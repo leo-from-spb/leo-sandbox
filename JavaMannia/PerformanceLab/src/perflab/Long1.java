b416ccd966b264cb5072d998bfe5f8f4ea0cb0a4
Leonid Bushuev <leonidos@gmail.com>
2014-11-27 19:10:08 +0100

    Longs: get rid of optimization

--- JavaMannia/PerformanceLab/src/perflab/Long1.java
+++ JavaMannia/PerformanceLab/src/perflab/Long1.java
@@ -19,22 +19,28 @@ public class Long1 {
     private static void MeasureIntCounter() {
         sleep(400);
         long timestamp1 = System.currentTimeMillis();
+        int x = 0;
         for (int a = 1; a < 100000000; a++) {
             for (int b = 1; b < 20; b++) {
+                x = (x >> 1) + (a - b);
             }
         }
         long time = System.currentTimeMillis() - timestamp1;
+        System.out.println("x = " + x);
         System.out.println("Int counters time: " + time);
     }
 
     private static void MeasureLongCounter() {
         sleep(400);
         long timestamp1 = System.currentTimeMillis();
+        long x = 0;
         for (long a = 1; a < 100000000; a++) {
             for (long b = 1; b < 20; b++) {
+                x = (x >> 1) + (a - b);
             }
         }
         long time = System.currentTimeMillis() - timestamp1;
+        System.out.println("x = " + x);
         System.out.println("Long counters time: " + time);
     }
 
